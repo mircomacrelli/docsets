a8fbfee7ae8676c5d4add5380f74c8879b07ac48
Mirco Macrelli <mail@mircomacrelli.net>
2023-11-04 21:28:28 +0100

    add a script to rebuild all changed docsets

---
+++ rebuild-changed.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for XML in $(git ls-files -m docsets/*.xml); do
+    YML=$(echo $XML | sed -e 's/docsets/configs/;s/xml$/yml/')
+    java -jar spring-reference-docset-builder-1.0-all.jar $YML docsets
+    DOCSET=$(echo $XML | sed -e 's/xml$/docset/;s/_/ /g;s/^docsets\///')
+    TGZ=$(echo $XML | sed -e 's/xml$/tgz/;s/_/ /g')
+    tar -C docsets -czvf "$TGZ" "$DOCSET"
+    rm -rf "docsets/$DOCSET"
+done
